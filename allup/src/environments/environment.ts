2c96efb33b5dad939c3417b4eecc58ab2eb0444e
AntonioVladimir <antortechin@gmail.com>
2021-02-23 19:39:37 +0100

    Firebase Init Configuration

--- allup/src/environments/environment.ts
+++ allup/src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAfQVmhhWN_kJnKrBoJ_jawZI4ew9MpIGM',
+    authDomain: 'allup-8a5c4.firebaseapp.com',
+    databaseURL:
+      'https://allup-8a5c4-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'allup-8a5c4',
+    storageBucket: 'allup-8a5c4.appspot.com',
+    messagingSenderId: '203225978400',
+    appId: '1:203225978400:web:6011edefb202935c0d4e45',
+    measurementId: 'G-N6VDZQWF9S',
+  },
 };
 
 /*
